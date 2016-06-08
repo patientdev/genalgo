bfe9316fb8e9249341aa16b02b59a658fd06c841
Shane Cavanaugh <shane@patientdev.co>
2016-06-08 18:12:31 -0400

    Simplistic plot of average time till desideratum

---
+++ plot.py
@@ -0,0 +1,25 @@
+import numpy
+import matplotlib.pyplot as pyplot
+import sqlite3 as db
+
+# Get info from db
+conn = db.connect('results.db')
+conn.row_factory = db.Row
+c = conn.cursor()
+
+averages = c.execute('SELECT given_number, avg(duration) from genalgo_results WHERE roulette_method = "stochastic_acceptance_roulette" group by given_number').fetchall()
+
+ind = numpy.arange(len(averages))
+width = 0.35
+
+fig, ax = pyplot.subplots()
+rects = ax.bar(ind, [r[1] for r in averages], width, color='b')
+
+# add some text for labels, title and axes ticks
+ax.set_xlabel('Desideratum')
+ax.set_ylabel('Average duration')
+ax.set_title('Average time till desideratum')
+ax.set_xticks(ind)
+ax.set_xticklabels((result['given_number'] for result in averages))
+
+pyplot.show()
