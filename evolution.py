8cc25cc3bcf4dfe2c4bb5284948ff1ace3ced412
Shane Cavanaugh <shane@patientdev.co>
2016-06-08 10:46:36 -0400

    Remove needless else: continue

--- evolution.py
+++ evolution.py
@@ -35,8 +35,6 @@ def stochastic_acceptance_roulette(population):
         random_genome = random.choice(population.genomes)
         if random.uniform(0, population.max_fitness) < random_genome.phenome.fitness / population.max_fitness:
             return random_genome
-        else:
-            continue
 
 
 def crossover(offspring_1_genome, offspring_2_genome):
