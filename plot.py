913744aef90924f497108195b253bf1e731f0006
Shane Cavanaugh <shane@patientdev.co>
2016-06-09 12:28:09 -0400

    Use # of generations till desiridatum instead of time

--- plot.py
+++ plot.py
@@ -7,7 +7,7 @@ conn = db.connect('results.db')
 conn.row_factory = db.Row
 c = conn.cursor()
 
-averages = c.execute('SELECT given_number, avg(duration) from genalgo_results WHERE roulette_method = "stochastic_acceptance_roulette" group by given_number').fetchall()
+averages = c.execute('SELECT given_number, avg(generations) from genalgo_results WHERE roulette_method = "stochastic_acceptance_roulette" group by given_number').fetchall()
 
 ind = numpy.arange(len(averages))
 width = 0.1
@@ -17,8 +17,8 @@ rects = ax.bar(ind, [r[1] for r in averages], width, color='cornflowerblue')
 
 # add some text for labels, title and axes ticks
 ax.set_xlabel('Desideratum')
-ax.set_ylabel('Average duration')
-ax.set_title('Average time till desideratum')
+ax.set_ylabel('Average # of generation')
+ax.set_title('Average # of generations till desideratum')
 ax.set_xticks(ind + (width/2))
 ax.set_xticklabels((result['given_number'] for result in averages), horizontalalignment='right')
 ax.set_xlim(-width)
