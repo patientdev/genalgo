e8dc290702f1ddda9802e8f9fe8aa8865068a120
Shane Cavanaugh <shane@patientdev.co>
2016-06-09 12:26:54 -0400

    Customize the look of the output graph and output to file

--- plot.py
+++ plot.py
@@ -10,16 +10,17 @@ c = conn.cursor()
 averages = c.execute('SELECT given_number, avg(duration) from genalgo_results WHERE roulette_method = "stochastic_acceptance_roulette" group by given_number').fetchall()
 
 ind = numpy.arange(len(averages))
-width = 0.35
+width = 0.1
 
 fig, ax = pyplot.subplots()
-rects = ax.bar(ind, [r[1] for r in averages], width, color='b')
+rects = ax.bar(ind, [r[1] for r in averages], width, color='cornflowerblue')
 
 # add some text for labels, title and axes ticks
 ax.set_xlabel('Desideratum')
 ax.set_ylabel('Average duration')
 ax.set_title('Average time till desideratum')
-ax.set_xticks(ind)
-ax.set_xticklabels((result['given_number'] for result in averages))
+ax.set_xticks(ind + (width/2))
+ax.set_xticklabels((result['given_number'] for result in averages), horizontalalignment='right')
+ax.set_xlim(-width)
 
-pyplot.show()
+fig.savefig('figures/avg.png')
